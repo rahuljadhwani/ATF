81f2c89166ccd1eeb1c953e0099fb26c66a98bfb
Deepender Rathore <deepenrathore28@gmail.com>
2020-05-09 04:27:17 +0530

    adding missing import org.openqa.selenium.StaleElementReferenceException

--- src/test/java/com/framework/atf/testcases/BasicTestCase.java
+++ src/test/java/com/framework/atf/testcases/BasicTestCase.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.testng.annotations.Listeners;
 
 import java.util.List;
